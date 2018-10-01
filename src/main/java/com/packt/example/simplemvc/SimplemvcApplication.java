2844d875dba2e2079170c8dd92d0c3fb47ee8f8b
Kostiuk_N <kost.nik88@gmail.com>
2018-10-01 09:03:07 +0300

    add linkedin

--- src/main/java/com/packt/example/simplemvc/SimplemvcApplication.java
+++ src/main/java/com/packt/example/simplemvc/SimplemvcApplication.java
@@ -3,10 +3,10 @@ package com.packt.example.simplemvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.packt.example"})
 public class SimplemvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplemvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimplemvcApplication.class, args);
+    }
 }
